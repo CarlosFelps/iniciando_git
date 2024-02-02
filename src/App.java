de80de08c620995801ec0e61e13c4b785e0cb78d
Carlos Phelipe <carlosfelipe037@gmail.com>
2024-02-01 21:34:01 -0300

    Adicionei uma nova lógica para o código

--- src/App.java
+++ src/App.java
@@ -1,19 +1,25 @@
 public class App {
     public static void main(String[] args) {
-      /*   
-        //Declarando Variavel
-        boolean a = true;
-        boolean b = false;
-        
-        // usando o operador XOR para verificar se apenas um dos valores é verdadeiro
-        boolean resultado = a ^ b;
+        /*
+         * //Declarando Variavel
+         * boolean a = true;
+         * boolean b = false;
+         * 
+         * // usando o operador XOR para verificar se apenas um dos valores é verdadeiro
+         * boolean resultado = a ^ b;
+         * 
+         * System.out.println("Resultado do XOR: " + resultado);
+         */
 
-        System.out.println("Resultado do XOR: " + resultado);
-    */
+        boolean gostaDeNike = true;
+        boolean gostaDeAdidas = false;
 
-        
+        if (gostaDeNike ^ gostaDeAdidas) {
+            System.out.println("Eu gosto de nike");
 
-
+        } else {
+            System.out.println("Eu não gosto de nike e sim Adidas");
+        }
 
     }
 }
